77d8569d710ebcfa1924f3cebb975935e77789c3
Archie Belarmino <110542367+acchiii@users.noreply.github.com>
2026-07-11 09:40:33 +0800

    Enhanced UI

---
+++ scanner.js
@@ -0,0 +1,173 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const output = document.getElementById('output');
+
+
+const ctx = canvas.getContext('2d');
+
+let questionsData = [];
+let lastAnswer = null;
+let interv;
+
+
+function loadData() {
+  try {
+    const raw = localStorage.getItem('data');
+    if (!raw) {
+      output.innerHTML = `<pre style="color:red;">⚠️ No JSON found in localStorage.</pre><br>`  + output.innerHTML;
+      return;
+    }
+
+    const parsed = JSON.parse(raw);
+
+    if (Array.isArray(parsed.questions)) {
+      questionsData = parsed.questions;
+      //console.log(`Loaded ${questionsData.length} questions`);
+    } else {
+      output.innerHTML = `<pre style="color:red;">⚠️ Invalid data format: ${JSON.stringify(parsed, null, 2)}</pre> <br> ` + output.innerHTML;
+    }
+  } catch (err) {
+    output.innerHTML = `<pre style="color:red;">JSON parse error:\n${err}</pre> <br>`  + output.innerHTML;
+  }
+}
+
+
+
+async function startCamera() {
+  try {
+   const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: { ideal: "environment" } },
+      audio: false
+    });
+    video.setAttribute('playsinline', true);
+    video.srcObject = stream;
+
+    video.play();
+  startScan()
+
+
+  } catch (err) {
+    //console.error("Camera access denied:", err);
+  }
+}
+
+
+async function scanFrame() {
+ 
+      
+  if(document.getElementById('cdat').style.display == 'none') return;
+//console.log('scanning')   
+
+  if (!questionsData.length) {
+    console.warn("No question data loaded");
+    return;
+  }
+
+  
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+
+  const result = await Tesseract.recognize(canvas, 'eng');
+  const detectedText = result.data.text.toLowerCase().trim();
+
+ 
+  const match = questionsData.find(q => {
+    const questionText = (q.question || '').toLowerCase();
+    return detectedText.includes(questionText.slice(0, 25));
+  });
+
+  if (match) {
+    if (lastAnswer !== match.answer) {
+      //console.log(`Detected Question: ${match.code}`);
+      //console.log(`Answer: ${match.answer}`);
+      output.innerHTML = `<br><span style="color:red; font-weight: bold;">Answer:  ${match.answer == '-' ? 'false' : match.answer}</span><br>` 
+      + `<span style="color:blue; ">Detected Question:</span> ${match.question}` ;
+      lastAnswer = `${match.answer == '-' ? 'false' : match.answer}`;
+
+       speak(`${match.answer == '-' ? 'false' : match.answer}`)
+    } else {
+      //console.log(` Skipped kay same answer.`);
+    }
+  } else {
+    //console.log("No match"); yellow
+  }
+}
+
+
+function startScan() {
+ interv = setInterval(scanFrame, 2000);
+}
+
+
+function speak(text) {
+  if (!text) return;
+  const utter = new SpeechSynthesisUtterance(text);
+  utter.lang = "en-US";         
+  utter.rate = 1;                
+  utter.pitch = 1;              
+  speechSynthesis.cancel();    
+  speechSynthesis.speak(utter);
+}
+
+
+(async function init() {
+  loadData();      
+  
+
+  document.body.addEventListener('keydown', (event) =>{
+    if(event.key == 'Enter' || event.key == 'Space'){
+      scanFrame();
+    }
+  });
+
+  document.body.addEventListener('click', function(){
+    scanFrame();
+  })
+  
+})();
+
+
+
+
+const se = document.getElementById('search');
+
+se.addEventListener('input', ()=>{
+  let sd = se.value;
+  if(!(sd.length>2 ) || !sd.includes(' '))  return;
+   const mm = questionsData.find(qq => {
+  const questionText = (qq.question || '').toLowerCase();
+  return questionText.includes(sd.toLowerCase());
+});
+
+  if (mm) {
+    if (lastAnswer !== mm.answer) {
+      output.innerHTML = `<br><span style="color:red; font-weight: bold;">Answer: ${mm.answer == '-' ? 'false' : mm.answer}</span><br>` 
+      + `<span style="color:blue; ">Detected Question:</span> ${mm.question}` ;
+      if(se.value.length > 0){ speak( `${mm.answer == '-' ? 'false' : mm.answer}`)}else {output.innerHTML = "Answer shows here...<br>(If you will use camera, focus the cam and touch the screen to answer)"}
+      lastAnswer = `${mm.answer == '-' ? 'false' : mm.answer}`;
+
+    }
+  }
+})
+
+
+
+
+document.getElementById('openCameraBtn').addEventListener('click', ()=>{
+  if(document.getElementById('cdat').style.display != 'none'){
+    document.getElementById('cdat').style.display = 'none';
+     if (video.srcObject) {
+    video.srcObject.getTracks().forEach(track => track.stop());
+    video.srcObject = null;
+  }
+    clearInterval(interv);
+   
+    
+  } else {
+     document.getElementById('cdat').style.display = 'flex';
+     startCamera();
+  }
+
+})
---
+++ script.js
@@ -0,0 +1,64 @@
+
+const result = document.getElementById('result');
+
+
+
+async function getExamData(code, result) {
+ 
+ 
+  try {
+    
+
+
+      try {
+         result.innerHTML += `<b style="color: orange;">...</b><br>`;
+        
+        const data = JSON.parse(localStorage.getItem('data'));
+
+        
+             if (data && Array.isArray(data.questions)) {
+         decode(data);
+      } else {
+        result.innerHTML += `<b style="color: orange;">No questions found or invalid format.</b><br>`;
+        
+        result.innerHTML += `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+      }
+
+
+      } catch (err) {
+        result.innerHTML += `\n<b style="color: red;">JSON parse error: ${err.message}</b><br>`;
+      }
+   
+  } catch (error) {
+    result.innerHTML += `\n<b style="color: red;">Fetch failed: ${error.message}</b><br>`;
+  }
+}
+function decode(data) {
+ data.questions.forEach((q, index) => {
+
+    let qq = `<span style="color: white;"><b>${index + 1}. ${q.question}</b></span><br>`;
+    let a = `<span style="color: red;"><span style="color: blue;">Answer:</span> <b><i>${q.answer == '-' ? 'false' : q.answer}</i></b></span><br><br><br>`;
+    result.innerHTML += qq + a;
+ });
+
+}
+function dsd(a) {
+ 
+  a = a.replace(/[\s\u200B-\u200D\uFEFF]+/g, '');
+  try {
+    return atob(a).trim();
+  } catch (e) {
+    console.error("Invalid Base64 string:", e);
+    return "";
+  }
+}
+
+
+
+
+
+
+ //result.innerHTML += `<b style="color: green;">Fetching Data of Subject Code: <span style="decoration: underline">${localStorage.getItem('code')}</span></b><br>`;
+        
+getExamData(localStorage.getItem('data'), result);
+
